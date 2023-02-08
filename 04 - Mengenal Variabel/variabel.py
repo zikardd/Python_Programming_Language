ea693d6b76996e6f4633891f1f5e4d2e059a0b30
Irzikri Naufaldi <89294307+zikardd@users.noreply.github.com>
2023-02-09 01:53:33 +0700

    Add files via upload

---
+++ 04 - Mengenal Variabel/variabel.py
@@ -0,0 +1,9 @@
+''' 
+Ini merupakan materi ketiga
+'''
+nama = 'zikri'
+
+print('halo dunia, my name is', nama)
+a = 7
+# Tidak boleh terdapat spasi pada sebuah variabel
+# Tidak boleh diawali dengan angka pada sebuah variabel
