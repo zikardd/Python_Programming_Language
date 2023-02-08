46311514714fcdcbca1aa123b98f858f312a6154
Irzikri Naufaldi <89294307+zikardd@users.noreply.github.com>
2023-02-09 01:46:46 +0700

    Add files via upload

---
+++ 03 - Cara Kerja Program dan Bytecode/main.py
@@ -0,0 +1,23 @@
+'''
+Ini merupakan materi kedua
+'''
+
+# Inisialisasi variabel 
+print('halo, apa kabar') # Menampilkan teks menggunakan fungsi print
+
+'''
+Bytecode yaitu hasil kompilasi python code menjadi c,
+bytecode berfungsi untuk mempercepat proses compile coding nya
+'''
+
+''' 
+Membuat bytecode, ketikan di terminal menggunakan syntax :
+python -m py_compile main.py
+'''
+
+'''
+Setelah berhasil di-compile, akan muncul folder baru
+yang memiliki nama __pycache__
+Untuk menjalankan pycache tersebut, gunakan syntax :
+python __pycache__/main.cpython-311.pyc
+'''
